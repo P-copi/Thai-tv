e3f60432ebd9e08298f41fd002b352192937411f
P-copi <Standardnewpro@gmail.com>
2026-09-09 11:29:20 +0700

    Add Color bridge for v2.1 activity

---
+++ ThaiTVPlayer-v2/app/src/main/java/com/pcopi/thaitv/Color.java
@@ -0,0 +1,8 @@
+package com.pcopi.thaitv;
+
+public final class Color {
+  private Color() {}
+  public static int rgb(int r,int g,int b){return android.graphics.Color.rgb(r,g,b);}
+  public static final int WHITE=android.graphics.Color.WHITE;
+  public static final int BLACK=android.graphics.Color.BLACK;
+}
